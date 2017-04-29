408a8d5b1ce1a25a07d87894cec2c54eac178bc8
Ivan Rimbaud <ivan.rimbaud@gmail.com>
2017-04-29 16:39:36 +0200

    integrate elum view into this project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+    skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def dashboard
   end
 end
